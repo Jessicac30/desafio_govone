9cb826477e4fc1b3ea391c34049f9e757212efb1
Jessica Carvalho <jmaria.carvalho30@gmail.com>
2025-01-27 17:15:30 -0300

    refactor: endpoints por classes

--- api_root/urls.py
+++ api_root/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('noticias/', include('api_rest.urls'), name='api_rest_urls'),
+    path('api/', include('api_rest.urls'), name='api_rest_urls'),
 
 ]
