7a233d436916c93229d5fe93b43056b960389109
Jessica Carvalho <jmaria.carvalho30@gmail.com>
2025-01-27 19:21:49 -0300

    chore: adicionando swagger

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 from .views import NewsList, NewsDetail
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+swagger_view = get_schema_view(
+    openapi.Info(
+        title="API de Notícias Govone",
+        default_version="V1",
+        description="Documentação da API para gerenciar notícias Govone"
+    ),
+)
 
 urlpatterns = [
     path('noticias/', NewsList.as_view(), name='news-list'),
     path('noticias/<uuid:pk>/', NewsDetail.as_view(), name='news-detail'),
+    path('swagger/', swagger_view.with_ui('swagger'))
 ]
