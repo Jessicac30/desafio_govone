a9f503420a1f1b2a94632a9de51ac3854eb65b49
Jessica Carvalho <jmaria.carvalho30@gmail.com>
2025-01-27 14:51:56 -0300

    chore: importando views

--- api_rest/views.py
+++ api_rest/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import News
+from .serializers import NewsSerializer
+
+import json
+
